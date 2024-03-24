6b98f0f2bc63d4e7fc2d4de6dffd2ab01b3401d4
Owidiusz Zieliński <blackcloud@MacBook-Air-Elzbieta.local>
2024-03-24 16:31:29 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.java2denginejavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.java2denginejavafx to javafx.fxml;
+    exports com.example.java2denginejavafx;
+}
